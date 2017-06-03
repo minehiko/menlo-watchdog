c89eaf8589428aaa8f06b53054856e0f87adb5b9
Minehiko Nohara <nohara-m@macnica.net>
2017-06-03 17:21:19 +0900

    Update menlotest.sh

--- menlotest.sh
+++ menlotest.sh
@@ -1,32 +1,73 @@
 #!/bin/sh
+# Log
+out="/var/log/menlotest.log"
+echo `date` "[INFO] MenloHealthcheck started" >> $out
 
 # Iteration Interval (sec)
 i=300
 
 # email settings
-smtp=""
-fromadd=""  # From email address
-toadd=""  # To email address (camma separated)
-mtitle="Menlo Alert!"  # Email title
-mbody="No Expected Access was found in past 5 mins!" # Email body
+smtp="smtp.gmail.com:587"
+fromadd="menlo2@cs.macnica.net"  # From email address
+toadd="nohara-m@macnica.net,sasaki-ta@macnica.net,tamaki-ta@macnica.net"  # To email address (camma separated)
+mtitle="[AWS-1][障害] Menlo Alert!"  # Email title
+mbody="[AWS-1] No Expected Access was found in past 5 mins!!" # Email body
 
-# Detection user and url
-url="https://hub.docker.com/r/minepicco/mailserver/tags/"
-usr=""
+# Squid setting
 
+filename=/home/ubuntu/menlo/squid3/access.log
+output=/home/ubuntu/menlo/tail200_accesslog.txt
+
+#fr=`date +%s` # Current time with unix format
+#cnt=0 # counter
+#freq=300 # monitoring threshold second
+
+# output tmp file
+
+#output=/home/ubuntu/menlo/tail200_accesslog.txt
+#tail -200 $filename > $output
+
+# Slack setting
+#msg=`/home/ubuntu/menlo/slack_notify.sh`
+
+# while do
 while :
 do
- a=`date +%s`
- b=$(( a-i ))
 
- curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"token":"***"}' 'https://logs.menlosecurity.com/api/rep/v1/fetch/client_select?format=JSON&start='$b | python -m json.tool > /home/nohara/menlo/menloout.txt
+ fr=`date +%s` # Current time with unix format
+ cnt=0 # counter
+ #freq=300 # monitoring threshold second
 
-## email alerting
- cnt=`sed -e '1d'  /home/***/menlo/menloout.txt | sed -e '$d' | jq .result.events | grep $url -B2 | grep $usr -c`
- if [ $cnt -lt 1 ]; then
-   /home/***/email/sendEmail-v1.56/sendEmail -f $fromadd -t $toadd -s $smtp -u $mtitle -m $mbody -a /home/***/menlo/menloout.txt
-   sed -e '1d'  /home/***/menlo/menloout.txt | sed -e '$d' | jq .result.events | grep $url -B2 | grep $usr -c
+ tail -200 $filename > $output
+
+ while read line
+ do
+
+   aaa=`echo $line | awk -F' ' '{print $1}'`
+
+   diff=`expr $fr - $aaa`
+
+   if  [ $diff -lt 300  ]
+   then
+       if [ "`echo "$line" | grep "response.php"`" ] ; then
+          echo ${line}
+          echo $cnt
+          cnt=`expr $cnt + 1`
+       fi
+   fi
+
+ done < $output
+
+# Email & Slack
+ echo `date` "[INFO] Verified " $cnt " Logs in the past" $i "sec" >> $out
+
+ if [ $cnt -lt 3 ]
+   then
+   /home/ubuntu/menlo/sendEmail-v1.56/sendEmail -o tls=yes -f $fromadd -t $toadd -s $smtp -xu menmac.mon@gmail.com -xp '!Menlo123' -u $mtitle -m $mbody -a /home/ubuntu/menlo/tail200_accesslog.txt
+   sh /home/ubuntu/menlo/slack_notify.sh
+
  fi
+ echo `date` "[INFO] Health check is finished. Proceeding another iteration." >> $out
  sleep $i
 
 done
